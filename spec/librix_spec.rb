76de22f3292cf919ea0e9157374ece4fca1cfef1
Vinicius Coelho <vinicius2001@gmail.com>
2025-10-16 20:15:07 -0300

    Initial commit: Add Librix gem structure with essential files including .gitignore, README, LICENSE, and basic gemspec. Implement RSpec for testing and setup scripts for development.

---
+++ spec/librix_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Librix do
+  it "has a version number" do
+    expect(Librix::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
